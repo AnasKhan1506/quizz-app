11086c99770f57196c9b1e509fbe6516081649b0
AnasKhan1506 <saifullahkha00@gmail.com>
2024-01-31 15:07:43 -0800

    initial screen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Color.fromARGB(255, 103, 39, 89),
+        ),
+      ),
+    );
+  }
+}
