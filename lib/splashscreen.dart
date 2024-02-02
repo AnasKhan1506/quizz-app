56e153fce645b4797d095b450480b575dde391e0
AnasKhan1506 <saifullahkha00@gmail.com>
2024-02-01 16:33:28 -0800

    complete initial screen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -7,8 +7,43 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          color: Color.fromARGB(255, 103, 39, 89),
+        backgroundColor: Color.fromARGB(255, 104, 13, 154),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 300,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Learn Flutter the fun way!",
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 244, 218, 255),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              style: ButtonStyle(elevation: MaterialStatePropertyAll(2)),
+              onPressed: () {},
+              child: Text(
+                "Start Quiz",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 244, 218, 255),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
         ),
       ),
     );
