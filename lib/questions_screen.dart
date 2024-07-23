ccea744b9fe5ccd1133ff55e0eceb9973b5fa66d
AnasKhan1506 <akanaskhan1506@gmail.com>
2024-07-22 18:21:51 -0700

    mutating memory

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizzapp/answer_button.dart';
+import 'package:quizzapp/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,19 +12,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("questions"),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(answerText: "answer 1", onTap: () {}),
-          AnswerButton(answerText: "answer 2", onTap: () {}),
-          AnswerButton(answerText: "answer 3", onTap: () {}),
-        ],
+    final currentQuestion = questions[0];
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
